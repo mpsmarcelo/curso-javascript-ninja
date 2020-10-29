9117cc2a522be05ad9883b3f51b1b6f91c3bbe3c
mpsmarcelo <marcellopsnet@gmail.com>
2020-10-29 07:59:00 -0300

    challenge-14

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -107,18 +107,28 @@ Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
 console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+var obj = numberObjects[1];
+if(numberObjects.indexOf(obj) > - 1){
+    console.log('Existe um objeto { number: 2 } em numberObjects!');
+}else{
+    console.log('Não existe um objeto { number: 2 } em numberObjects :(');
+}
 
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
 será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
 console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-// ?
+if(numberObjects.lastIndexOf((obj),2) > - 1){
+    console.log('Existe um objeto { number: 2 } em numberObjects!');
+}else{
+    console.log('Não existe um objeto { number: 2 } em numberObjects :(');
+}
+
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
 console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+console.log(Array.isArray(justMod2Or3))
